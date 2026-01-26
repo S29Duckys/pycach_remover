ad8731f4d20f3fc709189459a3077d550f34d2fb
S29Ducky <nbarasraso@edenschool.fr>
2026-01-26 11:21:24 +0100

    feat (app) : mise à jour de la fonction is_admin et ajout d'une fonction clear

--- src/pycache_remove/main.py
+++ src/pycache_remove/main.py
@@ -1,20 +1,31 @@
+import os
 import ctypes
+import time
 
+# Function for clear the console
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
 
+# Function to check for admin privileges
 def is_admin():
     try: 
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
 
-
+# Verify admin privileges
+def verify_admin():
+        if is_admin():
+            print("Running with admin privileges. Waiting for 2 seconds...")
+            time.sleep(2)
+            main()
+        else:
+            print("you must run this script in admin mode")
 
 # Main menu
 def main():
-    if is_admin():
-        print("Running with admin privileges.")
-    else:
-        print("you must run this script in admin mode")
+    clear()
+    print("Welcome to the PyCache Remove Tool")
 
 if __name__ == "__main__":    
-    main()
+    verify_admin()
