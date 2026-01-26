f4939d82f68c40f8a194eb4ac56016da9cb75a27
S29Ducky <nbarasraso@edenschool.fr>
2026-01-26 11:55:53 +0100

    feat(app): implementation of the function to delete the __pycache__ directory

--- src/pycache_remove/main.py
+++ src/pycache_remove/main.py
@@ -1,6 +1,8 @@
 import os
-import ctypes
 import time
+import shutil
+import ctypes
+
 
 # Function for clear the console
 def clear():
@@ -20,12 +22,28 @@ def verify_admin():
             time.sleep(2)
             main()
         else:
-            print("you must run this script in admin mode")
+            print("You must run this script in admin mode")
+
+def remove_pycache_dirs(target_path):
+    if not os.path.isdir(target_path):
+        print("The provided path is not a valid directory.")
+        return
+    for current_path, dirs, files in os.walk(target_path):
+        if "__pycache__" in dirs:
+            pycache_path = os.path.join(current_path, "__pycache__")
+            try: 
+                shutil.rmtree(pycache_path)
+                print(f"Supprimé : {pycache_path}")
+            except Exception as e:
+                print(f"Erreur avec {pycache_path} : {e}")
 
 # Main menu
 def main():
     clear()
-    print("Welcome to the PyCache Remove Tool")
+    print("Welcome to the PyCache Remove Tool \n")
+    path = input("Enter the directory path to remove __pycache__ folders from: ")
+
+    remove_pycache_dirs(path)
 
 if __name__ == "__main__":    
     verify_admin()
